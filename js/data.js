8c4ae7c563beb53a75a5ea0509bb4418b259762e
dvid <contacto@dvidsilva.com>
2013-09-08 12:59:20 -0700

    new data added

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 var products = new Array();
 var types = {};
 types['shirt'] = ["cocktail-dresses", "evening-dresses", "dresses", "day-dresses", "bridal-bridesmaid-dresses",  "wedding-dresses",  "bridesmaid-dresses", "mother-bride-dresses", "cocktail-dresses", "petite-sweatshirts", "petite-sweaters", "jackets", "leather-jackets"];
-types['pants'] = ["athletic-skirts", "maternity-skirts", "petite-skirts","mini-skirts", "skinny-jeans", "jeans", "Worn Jeans", "stretch-jeans", "denim", "straight-leg-jeans"];
+types['pants'] = ["athletic-skirts", "maternity-skirts", "petite-skirts","mini-skirts", "skinny-jeans", "jeans", "Worn Jeans", "stretch-jeans", "denim", "straight-leg-jeans", "long-skirts", "mid-length-skirts"];
 types['shoes'] = [];
 types['accesories'];
 
